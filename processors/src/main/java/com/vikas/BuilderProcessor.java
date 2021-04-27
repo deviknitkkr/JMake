0706dc1f88dc18dc9d92ef56043beee0f358df4b
deviknitkkr <deviknitkkr3@gmail.com>
2021-04-27 22:42:21 +0530

    initial commit

--- processors/src/main/java/com/vikas/BuilderProcessor.java
+++ processors/src/main/java/com/vikas/BuilderProcessor.java
@@ -12,33 +12,37 @@ import java.util.Set;
 
 
 @AutoService(Processor.class)
-@SupportedSourceVersion(value = SourceVersion.RELEASE_15)
-
 public class BuilderProcessor extends AbstractProcessor {
 
     Messager messager;
+
     public BuilderProcessor() {
     }
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
-        messager=processingEnv.getMessager();
-        messager.printMessage(Diagnostic.Kind.MANDATORY_WARNING,"Annotation processing started...");
+        messager = processingEnv.getMessager();
+        messager.printMessage(Diagnostic.Kind.MANDATORY_WARNING, "Annotation processing started...");
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        for(Element element: roundEnv.getElementsAnnotatedWith(Builder.class)){
-            messager.printMessage(Diagnostic.Kind.WARNING,"Processing:"+element.getSimpleName());
+        for (Element element : roundEnv.getElementsAnnotatedWith(Builder.class)) {
+            messager.printMessage(Diagnostic.Kind.WARNING, "Processing:" + element.getSimpleName());
         }
         return true;
     }
 
     @Override
     public Set<String> getSupportedAnnotationTypes() {
-        Set<String> set=new LinkedHashSet<>();
+        Set<String> set = new LinkedHashSet<>();
         set.add(Builder.class.getCanonicalName());
         return set;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
     }
 }
